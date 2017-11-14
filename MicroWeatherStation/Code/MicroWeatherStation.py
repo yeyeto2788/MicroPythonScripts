a3257014af6679a08b4d90aaafbb0983e31b0b89
Juan Biondi <juanernestobiondi@gmail.com>
2017-11-14 12:26:43 +0100

    Create MicroWeatherStation.py

---
+++ MicroWeatherStation/Code/MicroWeatherStation.py
@@ -0,0 +1,59 @@
+import socket
+import dht
+import machine
+
+html = b"""<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Weather Station</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>ESP8266 Weather Station</h1>
+<table><tr><th>Temperature</th><th>Humidity</th></tr>%s</table>
+</body>
+</html>
+"""
+
+def ReadSensors():
+    """
+    Read the DHT11 sensor for Temperature and
+
+    Returns:
+            An array with the temperature and the humidity.
+    """
+    d = dht.DHT11(machine.Pin(4))
+    Temperature = d.temperature()
+    Humidity = d.humidity()
+    data = "<tr><td>%s ºC</td><td>%s %</td></tr>" % (Temperature, Humidity)
+    return data
+
+
+def main():
+    """
+    Main code to be executed, all logic it is within this function.
+
+    Returns:
+            None
+    """
+    addr = socket.getaddrinfo('192.168.4.1', 80)[0][-1]
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(5)
+
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            h = cl_file.readline()
+            if h == b"" or h == b"\r\n":
+                break
+        rows = ReadSensors()
+        response = html % '\n'.join(rows)
+        cl.send(response)
+        cl.close()
+
+main()
