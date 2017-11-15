fc18689625849f68c6c7dfc84f293c0521212f80
Juan Biondi <juanernestobiondi@gmail.com>
2017-11-15 15:55:47 +0100

    Create boot.py

---
+++ MicroBoard/Release/boot.py
@@ -0,0 +1,16 @@
+import gc
+import os
+
+def CheckMSG_File():
+    Files = os.listdir()
+    MSG_file = 'messages.txt'
+    with open() as f:
+        Data = f.readlines()
+        DataLenght = len(Data)
+        f.close()
+    if DataLenght > 10:
+        with open(MSG_file,'w') as f:
+            f.close()
+
+gc.collect()
+CheckMSG_File()
