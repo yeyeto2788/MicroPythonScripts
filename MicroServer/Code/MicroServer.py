9a4a1183febd20c81ecc272bbead24eeeb901b3a
Juan Biondi <juanernestobiondi@hotmail.com>
2018-01-15 10:18:54 +0100

    Add new project

---
+++ MicroServer/Code/MicroServer.py
@@ -0,0 +1,29 @@
+import picoweb
+import network
+
+app = picoweb.WebApp("app")
+
+@app.route("/")
+def index(req, resp):
+    yield from picoweb.start_response(resp, content_type = "text/html")
+
+    htmlFile = open('index.html', 'r')
+
+    for line in htmlFile:
+      yield from resp.awrite(line)
+
+
+def getIP():
+    wlan = network.WLAN(network.STA_IF)
+    if wlan.active():
+        addr = wlan.ifconfig()[0]
+    else:
+        wlan = network.WLAN(network.AP_IF)
+        if wlan.active():
+            addr = wlan.ifconfig()[0]
+        else:
+            print("No active connection")
+            return addr
+
+
+app.run(debug=True, host = getIP)
