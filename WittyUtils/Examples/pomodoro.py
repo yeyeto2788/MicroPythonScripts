59754e999efa99c8f338a8e4e262f6cba920ac36
Juan Biondi <juanernestobiondi@gmail.com>
2018-03-05 16:47:04 +0100

    Update pomodoro.py

--- WittyUtils/Examples/pomodoro.py
+++ WittyUtils/Examples/pomodoro.py
@@ -5,6 +5,16 @@ witty = WittyBoard()
 
 
 def toggle_led(intmins, strled):
+    """
+    Toggles an LED for a given amount of minutes.
+
+    Args:
+        intmins: Integer with the minutes to toggle the led.
+        strled: String with the name of the led.
+
+    Returns:
+        Nothing.
+    """
     initvalue = 0
     initialTime = time.ticks_ms()
     itertime = initialTime
@@ -16,10 +26,13 @@ def toggle_led(intmins, strled):
             itertime = time.ticks_ms()
     witty.clear_leds()
 
+
+witty.clear_leds()
 while True:
     if not witty.read_button():
+        witty.set_boolean_value('blue_led', 0)
         toggle_led(25, "red_led")
         toggle_led(5, "green_led")
     else:
         pass
-
+        witty.set_boolean_value('blue_led', 1)
