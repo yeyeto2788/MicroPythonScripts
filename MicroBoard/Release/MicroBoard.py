c500e003765d4296668e281f27c8e7a46bff9f2f
Juan Biondi <juanernestobiondi@hotmail.com>
2018-05-30 13:37:24 +0200

    Add enhancement #10, lynt the code and update image.

--- MicroBoard/Release/MicroBoard.py
+++ MicroBoard/Release/MicroBoard.py
@@ -8,12 +8,12 @@ def check_file():
     if file_name in os.listdir():
         return True
     else:
-        with open(file_name, 'w') as f:
+        with open(file_name, "w") as f:
             f.close()
         return True
 
 def get_file_size():
-    with open(file_name, 'r') as f:
+    with open(file_name, "r") as f:
         lines = f.readlines()
         f.close()
         return len(lines)
@@ -32,13 +32,13 @@ def quick_decode(strdecode):
     return strdecode
 
 def write_file(strMessage):
-    if (strMessage != '#') or (len(strMessage) > 2):
+    if (strMessage != "#") or (len(strMessage) > 2):
         strMessage = quick_decode(strMessage)
         total_messages = read_file()
         if get_file_size() > 15:
             del total_messages[0]
         total_messages.append(strMessage)
-        with open(file_name, 'w') as f:
+        with open(file_name, "w") as f:
             for item in total_messages:
                 f.write(item + "\n")
             f.close()
@@ -53,57 +53,59 @@ def read_file():
                     message = line.rstrip()
                     filedata.append(message)
             else:
-                filedata.append('NO MESSAGES')
+                filedata.append("NO MESSAGES")
     return filedata
 
 def linted_data():
     data = []
     for element in read_file():
-        data.append('<tr>\n<td>%s</td>\n</tr>' % element)
+        data.append("<tr>\n<td>%s</td>\n</tr>" % element)
     return data
 
 
 def main():
     check_file()
-    html = """<!DOCTYPE html>
+    html ="""<!DOCTYPE html>
         <html lang="en">
         <head>
         <title>Message board</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
-        body              { font-family: 'Arial'; background: #fff; }
-        .h1_div			  { background-color: #424242; height: 90px;  line-height: 90px;  text-align: center; font-family: 'Arial';}
+        body              { font-family: "Arial"; background: #fff;}
+        .h1_div			  { background-color: #424242; height: 90px; text-align: center; font-family: "Arial";}
+        .current_time     { vertical-align:middle; font-family:"Arial";}
         h1                { border-bottom: 1px; }
-        h3                { font-family: 'Arial'; color: #424242; text-align: center;}
-        table             { table-layout: center;	font-family: 'Arial';}
+        h3                { font-family: "Arial"; color: #424242; text-align: center;}
+        table             { table-layout: center;	font-family: "Arial";}
         th                { font-style: bold; text-align: center; height: 15px; border-bottom: 1px solid #ddd; padding: 5px}
         td                { border-bottom: 1px solid #ddd; text-align: left}
         input[type=text]  { border: 1px solid #424242; margin: 4px 2px; width:20em;}
         input[type=text]:focus { border: 3px solid #424242; margin: 4px 2px; -webkit-transition: width 0.4s ease-in-out; transition: width 0.4s ease-in-out;}
-        input[type=submit]{ background-color: #424242; border: 3px; color: #ffffff; padding: 4px 8px; text-decoration: none; margin: 4px 2px; cursor: pointer; width:20em; font-weight: bold; font-family: 'Arial';}
+        input[type=submit]{ background-color: #424242; border: 3px; color: #ffffff; padding: 4px 8px; text-decoration: none; margin: 4px 2px; cursor: pointer; width:20em; font-weight: bold; font-family: "Arial";}
         form              {text-align: center;}
         </style>
         </head>
         <body>
         <div>
         <div class="h1_div">
-        <h1 align='center' style="color:#ffffff;">Message board</h1><br>
-        </div>
-        <div>
-        <table align='center'><tr><th><h1>Messages</h1></th></tr>%s</table>
+        <h1 align="center" style="color:#ffffff;">Message board</h1>
+        <h3 align="center" style="color:#ffffff;" class="current_time" id="humanTime"></h3>
+        </div><div>
+        <table align="center"><tr><th><h1>Messages</h1></th></tr>%s</table>
         <form>
         <br><h3>Type a message:</h3><br>
         <div><input type="text" name="messageinput"></input></div>
         <div><input type="submit" value="Send"></input></div>
-        </form>
-        </div>
-        </div>
+        </form></div></div>
+        <script>
+        function currentTime(){var e=new Date;datestring=e.toDateString()+" "+e.toTimeString(),document.getElementById("humanTime").innerHTML=datestring.split(" ").slice(0,5).join(" ")}var element=document.getElementById("humanTime");void 0!==element&&null!=element&&(window.load=setInterval(currentTime,1e3));
+        </script>
         </body>
         </html>
         """
 
-    addr = socket.getaddrinfo('192.168.4.1', 80)[0][-1]
+    addr = socket.getaddrinfo("192.168.4.1", 80)[0][-1]
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(addr)
@@ -112,26 +114,27 @@ def main():
 
     while True:
         cl, addr = s.accept()
-        print(connection_count, 'connection on', addr)
-        gc.mem_free()
+        print(connection_count, "connection on", addr)
+        print("Free in: %d" % gc.mem_free())
         connection_count += 1
-        cl_file = cl.makefile('rwb', 0)
+        cl_file = cl.makefile("rwb", 0)
         while True:
             h = cl_file.readline()
             gotten_msg = b"GET /?messageinput="
             if gotten_msg in h:
-                msg = h.decode('utf-8').split('/?messageinput=')
+                msg = h.decode("utf-8").split("/?messageinput=")
                 final_msg = msg[1][:(len(msg)-12)]
                 write_file(final_msg)
             if h == b"" or h == b"\r\n":
                 break
         rows = linted_data()
-        response = html % '\n'.join(rows)
+        response = html % "\n".join(rows)
         try:
             cl.sendall(response)
         except OSError as error:
             pass
         cl.close()
-        gc.mem_free()
+        print("Free out: %d" % gc.mem_free())
+
 
 main()
