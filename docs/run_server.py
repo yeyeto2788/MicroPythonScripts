4af5798353544cb574965984f62fc678fb27b071
Juan Biondi <juanernestobiondi@gmail.com>
2019-06-18 22:31:52 +0200

    Fix simple test to have a maximum of key presses

--- docs/run_server.py
+++ docs/run_server.py
@@ -6,10 +6,10 @@ It uses waitress to handle all WSGI related.
 """
 from waitress import serve
 
-from docs.presentation_server import app
+from presentation_server import app
 
 # Configuration of the server
-config = {'ip': '0.0.0.0', 'port': 80}
+config = {'ip': '0.0.0.0', 'port': 8080}
 
 # Generate a new Key with os.urandom(16) if you want to change it.
 app.secret_key = '3ZX\xb336\x15\x82/\xd5N1O\n\x9f\x8a'
