ede4446b98ed0b92e60fe67a5c5270b406fdf8d3
yeyeto2788 <juanernestobiondi@gmail.com>
2020-04-14 16:45:25 +0200

    Add more functions on the camera_slider project for further improvements.

--- projects/camera_slider/code/camera_slider.py
+++ projects/camera_slider/code/camera_slider.py
@@ -10,19 +10,63 @@ end_switch = Pin(5, Pin.IN, Pin.PULL_UP)
 
 SLOW = 5500
 FAST = 4000
+FORWARD = 1
+BACKWARD = -1
 motor = Stepper(HALF_STEP, 15, 13, 12, 14, delay=FAST)
 
 
+def fast_move(fn):
+    original = motor.delay
+
+    def wrapper(*args, **kwargs):
+        motor.delay = FAST
+        value = fn(*args, **kwargs)
+        return value
+
+    motor.delay = original
+    return wrapper
+
+
+def slow_move(fn):
+    original = motor.delay
+
+    def wrapper(*args, **kwargs):
+        motor.delay = SLOW
+        value = fn(*args, **kwargs)
+        return value
+
+    motor.delay = original
+    return wrapper
+
+
+@fast_move
 def home_motor():
     """Home the motor to its starting position.
 
     The motor will start spindle until the end switch is activated.
     """
     while start_switch.value():
-        motor.step(1, -1)
+        motor.step(1, BACKWARD)
 
     motor.step(10)
     motor.current_position = 0
+
+
+@fast_move
+def get_slider_size():
+    """Get the number of steps to get to the end of the slider.
+
+    It will first home the motor and start recording the steps to get
+    to the end of the slider and then home the motor again.
+    """
+    home_motor()
+
+    while end_switch.value():
+        motor.step(1)
+
+    slider_length = motor.current_position
+    home_motor()
+    return slider_length
 
 
 def timed_steps(steps, secs, count, callback=None):
@@ -45,3 +89,39 @@ def timed_steps(steps, secs, count, callback=None):
             break
 
         time.sleep(secs)
+
+
+def go_to(destination):
+    """Move the motor to a desired step number.
+
+    The movement will be perform based on the current position of
+    the motor.
+    """
+    current_pos = motor.current_position
+
+    if current_pos > destination:
+        movement = current_pos - destination
+        direction = BACKWARD
+    else:
+        movement = destination - current_pos
+        direction = FORWARD
+    motor.step(movement, direction)
+
+
+def move_to(start, end):
+    """Perform a movement from point A to point B.
+
+    First go to point A and then perform the movement to point
+    B.
+    """
+    go_to(start)
+
+    if start < end:
+        direction = FORWARD
+        x1 = start
+        x2 = end
+    else:
+        direction = BACKWARD
+        x1 = end
+        x2 = start
+    motor.step(x2 - x1, direction)
--- projects/camera_slider/release/camera_slider.py
+++ projects/camera_slider/release/camera_slider.py
@@ -5,12 +5,39 @@ start_switch=Pin(4,Pin.IN,Pin.PULL_UP)
 end_switch=Pin(5,Pin.IN,Pin.PULL_UP)
 SLOW=5500
 FAST=4000
+FORWARD=1
+BACKWARD=-1
 motor=Stepper(HALF_STEP,15,13,12,14,delay=FAST)
+def fast_move(fn):
+ original=motor.delay
+ def wrapper(*args,**kwargs):
+  motor.delay=FAST
+  value=fn(*args,**kwargs)
+  return value
+ motor.delay=original
+ return wrapper
+def slow_move(fn):
+ original=motor.delay
+ def wrapper(*args,**kwargs):
+  motor.delay=SLOW
+  value=fn(*args,**kwargs)
+  return value
+ motor.delay=original
+ return wrapper
+@fast_move
 def home_motor():
  while start_switch.value():
-  motor.step(1,-1)
+  motor.step(1,BACKWARD)
  motor.step(10)
  motor.current_position=0
+@fast_move
+def get_slider_size():
+ home_motor()
+ while end_switch.value():
+  motor.step(1)
+ slider_length=motor.current_position
+ home_motor()
+ return slider_length
 def timed_steps(steps,secs,count,callback=None):
  for iteration in range(count):
   motor.step(steps)
@@ -19,3 +46,23 @@ def timed_steps(steps,secs,count,callback=None):
   if iteration==(count-1):
    break
   time.sleep(secs)
+def go_to(destination):
+ current_pos=motor.current_position
+ if current_pos>destination:
+  movement=current_pos-destination
+  direction=BACKWARD
+ else:
+  movement=destination-current_pos
+  direction=FORWARD
+ motor.step(movement,direction)
+def move_to(start,end):
+ go_to(start)
+ if start<end:
+  direction=FORWARD
+  x1=start
+  x2=end
+ else:
+  direction=BACKWARD
+  x1=end
+  x2=start
+ motor.step(x2-x1,direction)
